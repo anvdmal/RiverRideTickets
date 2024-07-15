56778041d22719c80ac323ecb1b0abe445bee6f0
anvdmal <alekseeva.ann04@mail.ru>
2024-07-16 00:45:25 +0300

    add departureTime to Voyage

--- src/main/java/org/example/entities/Voyage.java
+++ src/main/java/org/example/entities/Voyage.java
@@ -3,24 +3,27 @@ package org.example.entities;
 import jakarta.persistence.*;
 
 import java.math.BigDecimal;
-import java.time.OffsetDateTime;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Set;
 
 @Entity
 @Table (name = "voyages")
 public class Voyage extends BaseEntityId {
     private Riverboat riverboat;
-    private OffsetDateTime departureDate;
+    private LocalDate departureDate;
+    private LocalTime departureTime;
     private BigDecimal basePrice;
     private String startPoint;
     private String endPoint;
     private int freeSeats;
     private Set<Ticket> ticketSet;
 
-    public Voyage(Riverboat riverboat, OffsetDateTime departureDate, BigDecimal basePrice,
+    public Voyage(Riverboat riverboat, LocalDate departureDate, LocalTime departureTime, BigDecimal basePrice,
                   String startPoint, String endPoint, Set<Ticket> ticketSet) {
         this.riverboat = riverboat;
         this.departureDate = departureDate;
+        this.departureTime = departureTime;
         this.basePrice = basePrice;
         this.startPoint = startPoint;
         this.endPoint = endPoint;
@@ -42,12 +45,21 @@ public class Voyage extends BaseEntityId {
     }
 
     @Column(nullable = false, name = "departure_date")
-    public OffsetDateTime getDepartureDate() {
+    public LocalDate getDepartureDate() {
         return departureDate;
     }
 
-    public void setDepartureDate(OffsetDateTime departure_date) {
+    public void setDepartureDate(LocalDate departure_date) {
         this.departureDate = departure_date;
+    }
+
+    @Column(nullable = false, name = "departure_time")
+    public LocalTime getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(LocalTime departureTime) {
+        this.departureTime = departureTime;
     }
 
     @Column(name= "base_price", nullable = false, scale = 2)
